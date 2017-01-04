db057778de3f8e41ff40048a1b9a2ed605dde379
Deboy <55631825@qq.com>
2017-01-04 23:50:56 +0800

    修复切换路由页面闪烁的BUG

--- src/utils/setWechatTitle.js
+++ src/utils/setWechatTitle.js
@@ -3,9 +3,9 @@ module.exports = function (title) {
   var mobile = navigator.userAgent.toLowerCase()
   if (/iphone|ipad|ipod/.test(mobile)) {
     var iframe = document.createElement('iframe')
-    iframe.style.visibility = 'hidden'
+    iframe.style.display = 'none'
     // 替换成站标favicon路径或者任意存在的较小的图片即可
-    iframe.setAttribute('src', '/static/logo.png')
+    iframe.setAttribute('src', '/favicon.ico')
     var iframeCallback = function () {
       setTimeout(function () {
         iframe.removeEventListener('load', iframeCallback)
